097ac535bd6d1ed29dc012566954ba84a9d70ab0
zoha <zoha@bylc.org>
2019-07-04 14:22:26 +0600

    added concurrency usign async

--- app/src/main/java/io/github/zoha131/coroutinelearning/MainActivity.kt
+++ app/src/main/java/io/github/zoha131/coroutinelearning/MainActivity.kt
@@ -8,7 +8,6 @@ import kotlinx.coroutines.*
 class MainActivity : AppCompatActivity() {
 
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -17,15 +16,29 @@ class MainActivity : AppCompatActivity() {
 
             Log.d("Coroutine", "Logging from coroutine. ${Thread.currentThread().name}")
 
-            logLater(2000)
+            asyncTask()
         }
     }
 
-    suspend fun logLater(millis: Long){
+    suspend fun asyncTask(): Int = coroutineScope {
+
+            Log.d("AsyncCoroutine", "Before calling")
+            val a = async { logLater(5000) }
+            val b = async { logLater(5000) }
+            Log.d("AsyncCoroutine", "After calling")
+
+            val c = a.await() + b.await()
+            Log.d("AsyncCoroutine", "Work completes here")
+
+            c
+        }
+
+
+    suspend fun logLater(millis: Long): Int {
 
         Log.d("Coroutine", "Logging outside of withcontext. ${Thread.currentThread().name}")
 
-        val result = withContext(Dispatchers.Default){
+        val result = withContext(Dispatchers.Default) {
 
             Log.d("Coroutine", "Logging before suspend. ${Thread.currentThread().name}")
             delay(millis)
@@ -33,8 +46,9 @@ class MainActivity : AppCompatActivity() {
 
             9
         }
-    }
 
+        return result
+    }
 
 
 }
