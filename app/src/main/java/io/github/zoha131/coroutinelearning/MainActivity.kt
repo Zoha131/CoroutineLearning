a194e3f2426802513aecb0d53f107c91041b78d7
zoha <zoha@bylc.org>
2019-07-04 13:50:49 +0600

    suspend function and coroutine creation

--- app/src/main/java/io/github/zoha131/coroutinelearning/MainActivity.kt
+++ app/src/main/java/io/github/zoha131/coroutinelearning/MainActivity.kt
@@ -2,11 +2,32 @@ package io.github.zoha131.coroutinelearning
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        GlobalScope.launch {
+            logLater(2000)
+        }
     }
+
+
+
+    suspend fun logLater(millis: Long){
+        Log.d("Coroutine", "Logging before suspend")
+        delay(millis)
+        Log.d("Coroutine", "Logging after suspend")
+    }
+
+
+
 }
