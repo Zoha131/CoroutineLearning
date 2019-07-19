298c48d3a4b39ff85e37bbaa8b7eb3148a8aa4bf
zoha131 <abirhasanzoha@gmail.com>
2019-07-19 23:20:59 +0600

    Handling exception on Deferred.await()

--- app/src/main/java/io/github/zoha131/coroutinelearning/MainActivity.kt
+++ app/src/main/java/io/github/zoha131/coroutinelearning/MainActivity.kt
@@ -1,11 +1,10 @@
 package io.github.zoha131.coroutinelearning
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.*
-import java.lang.NumberFormatException
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,24 +19,16 @@ class MainActivity : AppCompatActivity() {
 
         lifecycleScope.launch {
 
-            launch(handler) { //This handler won't have any effect
-                var value = 0
-
-                while (true){
-                    delay(1000)
-                    Log.d("LifecycleScope", "From second coroutine : ${++value}")
-                    errorTrial() // throw NumberFormatException("Exception Trial")
-                }
+            val result: Deferred<Unit> = async {
+                delay(1000)
+                Log.d("LifecycleScope", "From second coroutine")
+                errorTrial() // throw NumberFormatException("Exception Trial")
             }
 
-            lifecycleScope.launch(handler) { //This handler will handle exception
-                var value = 0
-
-                while (true){
-                    delay(1000)
-                    Log.d("LifecycleScope", "From second coroutine : ${++value}")
-                    errorTrial() // throw NumberFormatException("Exception Trial")
-                }
+            try {
+                result.await()
+            } catch (throwable: NumberFormatException){
+                Log.d("LifecycleScope", "$throwable")
             }
 
         }
@@ -45,7 +36,7 @@ class MainActivity : AppCompatActivity() {
         lifecycleScope.launch(handler) {
             var value = 0
 
-            while (true){
+            while (true) {
                 delay(1000)
                 Log.d("LifecycleScope", "From second coroutine : ${++value}")
                 errorTrial() // throw NumberFormatException("Exception Trial")
@@ -55,7 +46,7 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-    suspend fun errorTrial(){
+    suspend fun errorTrial() {
         Log.d("LifecycleScope", "Before Throwing Exception")
         delay(1000)
         throw NumberFormatException("Exception Trial")
@@ -64,16 +55,16 @@ class MainActivity : AppCompatActivity() {
     //coroutine with concurrency
     suspend fun asyncTask(): Int = coroutineScope {
 
-            Log.d("AsyncCoroutine", "Before calling")
-            val a = async { logLater(5000) }
-            val b = async { logLater(5000) }
-            Log.d("AsyncCoroutine", "After calling")
+        Log.d("AsyncCoroutine", "Before calling")
+        val a = async { logLater(5000) }
+        val b = async { logLater(5000) }
+        Log.d("AsyncCoroutine", "After calling")
 
-            val c = a.await() + b.await()
-            Log.d("AsyncCoroutine", "Work completes here")
+        val c = a.await() + b.await()
+        Log.d("AsyncCoroutine", "Work completes here")
 
-            c
-        }
+        c
+    }
 
     //coroutine without concurrency
     /*suspend fun asyncTask(): Int {
